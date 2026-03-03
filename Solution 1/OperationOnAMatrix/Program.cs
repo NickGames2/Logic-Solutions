3f8d5f4f1463a46502bd4d74fba6c22be8fe25bb
NickGames2 <jlagudelo5@gmail.com>
2026-03-02 19:59:58 -0500

    Solution exercise 30

---
+++ Solution 1/OperationOnAMatrix/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.Write("Ingrese orden de la matriz: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] mat = new int[n, n];
+int sum = 0;
+int may = -999999;
+int men = 999999;
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        mat[i, j] = (i + 1) - j;
+
+        Console.Write(mat[i, j] + "\t");
+
+        sum += mat[i, j];
+
+        if (mat[i, j] > may)
+        {
+            may = mat[i, j];
+        }
+
+        if (mat[i, j] < men)
+        {
+            men = mat[i, j];
+        }
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\nSuma total: " + sum);
+Console.WriteLine("Valor máximo: " + may);
+Console.WriteLine("Valor mínimo: " + men);
+
+Console.ReadKey();
---
+++ Solution 1/ShippingOfGoods/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Ingrese el peso de la mercancia: ");
+var mercancyString = Console.ReadLine();
+int mercancyInt = int.Parse(mercancyString);
+
+Console.Write("Valor de la mercancia: ");
+var VolummecancyString = Console.ReadLine();
+int VolummercancyInt = int.Parse(VolummecancyString);
+
+int Feextra = mercancyInt - 200;
+int Fee1 = 20000;
+int Fee2 = 25000;
+int Fee3 = 30000;
+int Fee4 = 35000;
+
+bool monday = true;
+bool Payment = true;
+
+
+if (mercancyInt < 100)
+{
+    Console.WriteLine($"La tarifa de venta es  {Fee1}");
+}
+ if (mercancyInt > 100 && mercancyInt < 150)
+{
+    Console.WriteLine($"La tarifa de venta es  {Fee2}");
+}
+ if (mercancyInt > 150 && mercancyInt <= 200)
+{
+    Console.WriteLine($"La tarifa de venta es  {Fee3}");
+}
+  if (mercancyInt > 200)
+{
+    Console.WriteLine($"La tarifa es de {Fee4 + (Feextra / 10) * 2000}");
+}
+
+
