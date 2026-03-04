0a6038d220b060d01a14577e1763af4d5fe1b8ea
NickGames2 <jlagudelo5@gmail.com>
2026-03-04 14:54:01 -0500

    E

---
+++ Solution 1/Factorization/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Ingrese el número a descomponer: ");
+if (int.TryParse(Console.ReadLine(), out int n))
+{
+    int original = n;
+    string factoresFormateados = ""; 
+    int divisor = 2;
+
+    Console.WriteLine("\nProceso de descomposición:");
+    Console.WriteLine("--------------------------");
+
+   
+    {
+        if (n % divisor == 0)
+        {
+            
+            Console.WriteLine($"{n,5} | {divisor}");
+
+            factoresFormateados += (factoresFormateados == "" ? "" : " x ") + divisor;
+
+            n /= divisor;
+        }
+        else
+        {
+            divisor++;
+        }
+    }
+
+    Console.WriteLine($"{1,5} |");
+    Console.WriteLine("--------------------------");
+
+    Console.WriteLine($"\nResultado: {original} = {factoresFormateados}");
+}
+else
+{
+    Console.WriteLine("Entrada no válida.");
+}
+
+Console.WriteLine("\nPresione cualquier tecla para salir...");
+Console.ReadKey();
