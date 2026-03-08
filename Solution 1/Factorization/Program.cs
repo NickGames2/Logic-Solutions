227b55a16b7b8da3ac99f8e4d3f73e9b22b31c76
NickGames2 <jlagudelo5@gmail.com>
2026-03-08 14:48:01 -0500

    Corrections

--- Solution 1/Factorization/Program.cs
+++ Solution 1/Factorization/Program.cs
@@ -9,28 +9,22 @@ if (int.TryParse(Console.ReadLine(), out int n))
     Console.WriteLine("\nProceso de descomposición:");
     Console.WriteLine("--------------------------");
 
-    // Este bucle se repite hasta que el número llega a 1
     while (n > 1)
     {
         if (n % divisor == 0)
         {
-            // Muestra la fila actual: Número | Divisor
             Console.WriteLine($"{n,5} | {divisor}");
 
-            // Guarda el factor para el mensaje final
             factoresFormateados += (factoresFormateados == "" ? "" : " x ") + divisor;
 
-            // Divide el número para seguir descomponiendo
             n /= divisor;
         }
         else
         {
-            // Si no es divisible por el actual, probamos con el que sigue (3, 4, 5...)
             divisor++;
         }
     }
 
-    // Al final siempre queda el 1 en la columna de la izquierda
     Console.WriteLine($"{1,5} |");
     Console.WriteLine("--------------------------");
 
